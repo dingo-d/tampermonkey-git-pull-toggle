b4d886d65bbeade2937ad4b36bffbc9b89607856
Denis Žoljom <dingo-d@users.noreply.github.com>
2018-06-25 18:26:03 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,40 @@
+// ==UserScript==
+// @name         Github collapse button
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Git collapse all button
+// @author       Denis Žoljom
+// @include      https://github.com/*
+// @match        http://*.github.com/*/pulls
+// @match        http://*.github.com/*/pulls/*
+// @match        https://*.github.com/*/pulls
+// @match        https://*.github.com/*/pulls/*
+// @require      https://code.jquery.com/jquery-3.3.1.min.js
+// @run-at       document-idle
+// ==/UserScript==
+
+(() => {
+  'use strict';
+
+  var element = $('.float-right.pr-review-tools');
+
+  if (typeof element !== 'undefined') {
+    element.prepend('<a href="#" style="margin-left:20px;" class="btn btn-sm tooltipped tooltipped-nw js-collapse-all" rel="nofollow" aria-label="Collapse all files">Collapse all files</a><a href="#" style="margin-left:20px;" class="btn btn-sm tooltipped tooltipped-nw js-expand-all" rel="nofollow" aria-label="Expand all files">Expand all files</a>');
+  }
+
+  $('.js-collapse-all').on('click', (event) => {
+    event.preventDefault();
+
+    $('.js-file:not(.open) .file-header > .file-actions > .js-details-target').each(function() {
+      $(this).click();
+    });
+  });
+
+  $('.js-expand-all').on('click', (event) => {
+    event.preventDefault();
+
+    $('.js-file.open .file-header > .file-actions > .js-details-target').each(function() {
+      $(this).click();
+    });
+  });
+})();
